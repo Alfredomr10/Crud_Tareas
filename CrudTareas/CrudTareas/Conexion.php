b0a69cdcf2f72285c74ce944c81967517c0460ae
Alfredo Maqueda Ruiz <98156274+Alfredomr10@users.noreply.github.com>
2023-03-23 13:32:18 +0100

    Add files via upload

---
+++ CrudTareas/CrudTareas/Conexion.php
@@ -0,0 +1,12 @@
+<?php
+try{
+    $base=new PDO('mysql:host=localhost; dbname=tareas;', 'root','');
+    $base->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $base->exec("SET CHARACTER SET UTF8");
+}catch(Exception $e){
+    die('Error' . $e->getMessage());
+    echo "Linea del error".$e->getLine();
+  }
+
+
+?>
